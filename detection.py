c59eb2c2c16a1b1e19273f34e2fbfc4e44335627
htv12 <160933745+htv12@users.noreply.github.com>
2024-03-24 09:04:17 -0400

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,20 +1,24 @@
 from roboflow import Roboflow
 import json
 
-
 rf = Roboflow(api_key="kKBzrSzGKIeyzfHSloBo")  # Roboflow API key
 project = rf.workspace().project("chiari-mri-detection")  # Roboflow project name
 model = project.version(3).model  # Roboflow version number
 
+
 # infer on a local image
 def infer(filePath):  # Run the defect detection model
-    return json.dumps(model.predict(filePath, confidence=77).json())  # Returns the detection data from the image
+    return model.predict(filePath, confidence=77).json()  # Returns the detection data from the image
 
-def parse(inference):
+
+def str_parse(inference):
     if 'chiari' in inference:  # Check if the inference contains chiari
         return 'Chiari'
     if 'syrinx' in inference:
         return 'Syrinx'
     if not 'syrinx' in inference or 'chiari' in inference:
         return 'Nothing Detected'
-    
+
+
+def dict_parse(inference):
+    return str(round(inference.get('predictions')[0].get('confidence'), 2))
