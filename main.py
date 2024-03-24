c74e4058bb1bfaf149278689c79a0df0bed56d4f
htv12 <160933745+htv12@users.noreply.github.com>
2024-03-24 01:18:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
+import ui
 import detection
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-inference = detection.infer(filename)
-print(detection.parse(inference))
